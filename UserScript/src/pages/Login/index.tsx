ecbb58aa0ee457a964383e7fc561a0ea0a59c126
mzl980425 <1198079622@qq.com>
2026-08-30 21:42:01 +0800

    chore: 移除 1.x 代码

--- UserScript/src/pages/Login/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import "./index.less";
-
-const Login: FC<{}> = ({}) => {
-  return null;
-};
-
-export default Login;
--- UserScript/src/utils/env.ts
+++
@@ -1,7 +0,0 @@
-import { unsafeWindow } from "$";
-import { version } from "../../package.json";
-
-export const appVersion =
-  (unsafeWindow || window).CTYunPCKeepAliveAppVersion || "0.0.0";
-
-export const scriptVersion = version;
